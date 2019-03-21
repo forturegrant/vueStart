c112b4210fd50eea4d48af3d109bb42895c93875
1209095474@qq.com <a158192305910>
2019-03-21 17:25:41 +0800

    first commit

---
+++ src/components/common/head.vue
@@ -0,0 +1,23 @@
+<template>
+  <header class="header">
+    <span>ele.me</span>
+    <span>登录|注册</span>
+  </header>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<style>
+  .header{
+    display: flex;
+    justify-content: space-between;
+  }
+</style>
---
+++ src/components/home.vue
@@ -0,0 +1,16 @@
+<template>
+  <head-top></head-top>
+</template>
+
+<script>
+  import headTop from './common/head'
+  export default {
+    data() {
+      return {}
+    },
+    components: {
+      headTop
+    }
+  }
+</script>
+
